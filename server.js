6fb85b3afa6d9f5692c4644bad71d33493cc171b
vishalkrsharma <vishalkrsharma1234@gmail.com>
2022-08-15 13:18:54 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+mongoose.connect('mongodb://localhost/urlShortener', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(process.env.PORT || 5000);
